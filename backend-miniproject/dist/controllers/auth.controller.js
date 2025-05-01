35eb4289605433de84770181673f0079e200a67f
Ferdy <ferdydjasni@gmail.com>
2025-05-02 00:31:47 +0700

    fix error 4

--- backend-miniproject/dist/controllers/auth.controller.js
+++ backend-miniproject/dist/controllers/auth.controller.js
@@ -1,4 +1,37 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -14,13 +47,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AuthController = void 0;
 const prisma_1 = __importDefault(require("../prisma"));
-const referral_codes_1 = __importDefault(require("referral-codes"));
 const bcrypt_1 = require("bcrypt");
 const jsonwebtoken_1 = require("jsonwebtoken");
 const mailer_1 = require("../helpers/mailer");
 const path_1 = __importDefault(require("path"));
 const fs_1 = __importDefault(require("fs"));
 const handlebars_1 = __importDefault(require("handlebars"));
+let referralCodeModule;
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    referralCodeModule = yield Promise.resolve().then(() => __importStar(require("referral-codes")));
+}))();
 class AuthController {
     registerCustomer(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
@@ -33,12 +69,11 @@ class AuthController {
                     res.status(400).send({ message: "Invalid referral code!" });
                     return;
                 }
-                const refCode = referral_codes_1.default
+                const refCode = referralCodeModule.default
                     .generate({
                     length: 7,
                     count: 1,
-                })
-                    .toString();
+                })[0];
                 const salt = yield (0, bcrypt_1.genSalt)(10);
                 const hashedPass = yield (0, bcrypt_1.hash)(password, salt);
                 const customer = yield prisma_1.default.customer.create({
