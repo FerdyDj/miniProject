6e49e5aad1893618e12a93254aa1893419529c68
Ferdy <ferdydjasni@gmail.com>
2025-05-02 01:10:12 +0700

    add backend link

--- frontend-miniproject/src/lib/axios.ts
+++ frontend-miniproject/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api";
+const BASE_URL = "https://be-hooppass.vercel.app/api";
 
 export default axios.create({
   baseURL: BASE_URL,
