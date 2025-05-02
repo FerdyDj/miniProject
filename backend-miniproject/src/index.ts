63538aa0609118c11aab346b84a38dd89f1a6408
Ferdy <ferdydjasni@gmail.com>
2025-05-02 09:12:49 +0700

    edit cors again

--- backend-miniproject/src/index.ts
+++ backend-miniproject/src/index.ts
@@ -18,7 +18,7 @@ const PORT: number = 8000;
 
 const app: Application = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: process.env.URL_FE}));
 
 app.get("/api", (req: Request, res: Response) => {
   res.status(200).send({
